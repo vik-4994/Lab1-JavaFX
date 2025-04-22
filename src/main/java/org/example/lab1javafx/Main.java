555dd5b188709d01759045aab1fc5aad5a833a5e
vik-4994 <igor.gavrylenko05@gmail.com>
2025-04-22 20:20:03 +0200

    Final version

--- src/main/java/org/example/lab1javafx/Main.java
+++ src/main/java/org/example/lab1javafx/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
